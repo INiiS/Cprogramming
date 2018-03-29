ddd3367bbf50656a9fd5a5c00287850632a51c2a
INiiS <sebsattler@live.fr>
2018-03-29 16:11:14 +0200

    Exercise 1-4 and for loop in Fahrenheit-Celsius table

---
+++ Fahrenheit-Celsius/CelsFahr.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/* print Celsius-Fahrenheit table
+ * for cels = 0, 20, ... , 300; */
+
+int main()
+{
+    float fahr, cels;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    cels = lower;
+    while(cels <= upper){
+        fahr = cels * 9 / 5 + 32;
+        printf("%3.0f %6.0f\n", cels, fahr);
+        cels = cels + step;
+    }
+}
