fbcc58983ac32831744cff188b68f694513968f8
INiiS <sebsattler@live.fr>
2018-03-29 17:08:30 +0200

    2nd version of count

--- IO/count.c
+++ IO/count.c
@@ -4,11 +4,11 @@
 
 int main()
 {
-    long nc;
+    double nc;
 
     nc = 0;
-    while(getchar() != EOF)
-        ++nc;
-    printf("%ld\n", nc);
+    for (nc = 0; getchar() != EOF; ++nc)
+        ;
+    printf("%.0f\n", nc);
 }
 
