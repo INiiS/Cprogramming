2932a5fa74c7659ae5119bbcc9b5c2a09fb55743
INiiS <sebsattler@live.fr>
2018-05-16 14:22:08 +0200

    Fixed all shit, and made the program work like it should.

--- IO/histogramFrequenciesCharacters.c
+++ IO/histogramFrequenciesCharacters.c
@@ -5,28 +5,37 @@
 
 #define FIRST_CHAR  32
 #define LAST_CHAR   127
-#define NB_CHAR     (LAST_CHAR - FIRST_CHAR) + 1 //96 
-
+#define NB_CHAR     128 //last char +1 to take into account array beginning at 0 
+#define FIRST_UPPER_CHAR    65
+#define LAST_UPPER_CHAR     90
+#define FIRST_LOWER_CHAR    97
+#define LAST_LOWER_CHAR     122
 int main(){
 
-    int i,input, counter;
-    int charFrequencies[NB_CHAR];
-    /* populate the array with zeros */
-    for( i = 0; i < NB_CHAR; ++i){
-        charFrequencies[i] = 0;
-    }
+    int i,input, letterIndex;
+    int charFrequencies[NB_CHAR] = {0};
+    
 
     while((input = getchar()) != EOF){
         if(input >= FIRST_CHAR && input <= LAST_CHAR){
-            ++charFrequencies[input - FIRST_CHAR]; // substract firstchar to c to get index position
+            ++charFrequencies[input]; 
         }        
     }
 
     /* Drawing the histogram */
-    for( int j = 0; j < NB_CHAR; ++j){
-        if((j+FIRST_CHAR >= 65 && j+FIRST_CHAR <= 90) || (j+FIRST_CHAR >=97 && j+FIRST_CHAR <=122)){
-            putchar(j+FIRST_CHAR);
-            printf(" | \n");
+    for( i = 0; i < NB_CHAR; ++i){
+        letterIndex = i + FIRST_CHAR;
+        if((letterIndex >= FIRST_UPPER_CHAR && letterIndex < LAST_UPPER_CHAR) || (letterIndex >= FIRST_LOWER_CHAR && letterIndex <= LAST_LOWER_CHAR)){ //printing letters
+
+            putchar(letterIndex);
+            printf(" | ");
+           // printf("TEST VALUE : %d   %d\n", letterIndex,charFrequencies[letterIndex]);
+            while(charFrequencies[letterIndex] > 0){
+                printf("*");
+                --charFrequencies[letterIndex];
+            }
+            printf("\n");
+
         }
         
     }
